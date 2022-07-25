89f9666f3c361df81d68ad7ede8773874aebd5ef
Yuan Zhang <Yuan.Zhang@audatex.com>
2022-07-25 11:31:22 +0800

    update generic test case format

--- src/test/java/com/example/mockserverdemo/MockServerGenericTests.java
+++ src/test/java/com/example/mockserverdemo/MockServerGenericTests.java
@@ -22,133 +22,133 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @TestInstance(Lifecycle.PER_CLASS)
 class MockServerGenericTests {
 
-    @Autowired
-    MockServerUtils mockServerUtils;
+  @Autowired
+  MockServerUtils mockServerUtils;
 
-    @AfterEach
-    void tearDown() {
-        mockServerUtils.reset();
-    }
+  @AfterEach
+  void tearDown() {
+    mockServerUtils.reset();
+  }
 
-    @Test
-    void testCreateBasicExpectation() {
-        MockServerUtils.client.when(request().withPath("/some/path"))
-                .respond(response().withBody("some_response_body"));
+  @Test
+  void testCreateBasicExpectation() {
+    MockServerUtils.client.when(request().withPath("/some/path"))
+        .respond(response().withBody("some_response_body"));
 
-        String body = given().log().all()
-                .then().log().all().statusCode(200)
-                .when().get("http://localhost:1080/some/path").asString();
-        Assertions.assertThat(body).isEqualTo("some_response_body");
-    }
+    String body = given().log().all()
+        .then().log().all().statusCode(200)
+        .when().get("http://localhost:1080/some/path").asString();
+    Assertions.assertThat(body).isEqualTo("some_response_body");
+  }
 
-    @Test
-    void testCreateUpdateExpectation() {
-        String expectationID = UUID.randomUUID().toString();
-        String bodyA = "AAA";
-        String bodyB = "BBB";
+  @Test
+  void testCreateUpdateExpectation() {
+    String expectationID = UUID.randomUUID().toString();
+    String bodyA = "AAA";
+    String bodyB = "BBB";
 
-        MockServerUtils.client
-                .when(request().withMethod("GET").withPath("/hello")).withId(expectationID)
-                .respond(response().withStatusCode(200).withBody(bodyA));
+    MockServerUtils.client
+        .when(request().withMethod("GET").withPath("/hello")).withId(expectationID)
+        .respond(response().withStatusCode(200).withBody(bodyA));
 
-        String body = given().log().all()
-                .then().log().all().statusCode(200)
-                .when().get("http://localhost:1080/hello").asString();
-        Assertions.assertThat(body).isEqualTo("AAA");
+    String body = given().log().all()
+        .then().log().all().statusCode(200)
+        .when().get("http://localhost:1080/hello").asString();
+    Assertions.assertThat(body).isEqualTo("AAA");
 
-        MockServerUtils.client
-                .when(request().withMethod("GET").withPath("/hello")).withId(expectationID)
-                .respond(response().withStatusCode(200).withBody(bodyB));
+    MockServerUtils.client
+        .when(request().withMethod("GET").withPath("/hello")).withId(expectationID)
+        .respond(response().withStatusCode(200).withBody(bodyB));
 
-        body = given().log().all()
-                .then().log().all().statusCode(200)
-                .when().get("http://localhost:1080/hello").asString();
-        Assertions.assertThat(body).isEqualTo("BBB");
-    }
+    body = given().log().all()
+        .then().log().all().statusCode(200)
+        .when().get("http://localhost:1080/hello").asString();
+    Assertions.assertThat(body).isEqualTo("BBB");
+  }
 
-    @Test
-    void testMatchingOrderCreationFirst() {
-        String bodyA = "AAA";
-        String bodyB = "BBB";
+  @Test
+  void testMatchingOrderCreationFirst() {
+    String bodyA = "AAA";
+    String bodyB = "BBB";
 
-        MockServerUtils.client.when(request().withMethod("GET").withPath("/hello"))
-                .respond(response().withStatusCode(200).withBody(bodyA));
+    MockServerUtils.client.when(request().withMethod("GET").withPath("/hello"))
+        .respond(response().withStatusCode(200).withBody(bodyA));
 
-        MockServerUtils.client.when(request().withMethod("GET").withPath("/hello"))
-                .respond(response().withStatusCode(200).withBody(bodyB));
+    MockServerUtils.client.when(request().withMethod("GET").withPath("/hello"))
+        .respond(response().withStatusCode(200).withBody(bodyB));
 
-        String body = given().log().all()
-                .then().log().all().statusCode(200)
-                .when().get("http://localhost:1080/hello").asString();
-        Assertions.assertThat(body).isEqualTo("AAA");
-    }
+    String body = given().log().all()
+        .then().log().all().statusCode(200)
+        .when().get("http://localhost:1080/hello").asString();
+    Assertions.assertThat(body).isEqualTo("AAA");
+  }
 
-    @Test
-    void testMatchingOrderPriorityFirst() {
-        String bodyA = "AAA";
-        String bodyB = "BBB";
+  @Test
+  void testMatchingOrderPriorityFirst() {
+    String bodyA = "AAA";
+    String bodyB = "BBB";
 
-        MockServerUtils.client.when(request().withMethod("GET").withPath("/hello")).withPriority(1)
-                .respond(response().withStatusCode(200).withBody(bodyA));
+    MockServerUtils.client.when(request().withMethod("GET").withPath("/hello")).withPriority(1)
+        .respond(response().withStatusCode(200).withBody(bodyA));
 
-        MockServerUtils.client.when(request().withMethod("GET").withPath("/hello")).withPriority(2)
-                .respond(response().withStatusCode(200).withBody(bodyB));
+    MockServerUtils.client.when(request().withMethod("GET").withPath("/hello")).withPriority(2)
+        .respond(response().withStatusCode(200).withBody(bodyB));
 
-        String body = given().log().all()
-                .then().log().all().statusCode(200)
-                .when().get("http://localhost:1080/hello").asString();
-        Assertions.assertThat(body).isEqualTo("BBB");
-    }
+    String body = given().log().all()
+        .then().log().all().statusCode(200)
+        .when().get("http://localhost:1080/hello").asString();
+    Assertions.assertThat(body).isEqualTo("BBB");
+  }
 
-    @Test
-    void testMatchingOrderTimesExactly() {
-        String bodyA = "AAA";
-        String bodyB = "BBB";
+  @Test
+  void testMatchingOrderTimesExactly() {
+    String bodyA = "AAA";
+    String bodyB = "BBB";
 
-        MockServerUtils.client
-                .when(request().withMethod("GET").withPath("/hello"), Times.exactly(1))
-                .respond(response().withStatusCode(200).withBody(bodyA));
+    MockServerUtils.client
+        .when(request().withMethod("GET").withPath("/hello"), Times.exactly(1))
+        .respond(response().withStatusCode(200).withBody(bodyA));
 
-        MockServerUtils.client
-                .when(request().withMethod("GET").withPath("/hello"), Times.exactly(1))
-                .respond(response().withStatusCode(200).withBody(bodyB));
+    MockServerUtils.client
+        .when(request().withMethod("GET").withPath("/hello"), Times.exactly(1))
+        .respond(response().withStatusCode(200).withBody(bodyB));
 
-        String body = given().log().all()
-                .then().log().all().statusCode(200)
-                .when().get("http://localhost:1080/hello").asString();
-        Assertions.assertThat(body).isEqualTo("AAA");
+    String body = given().log().all()
+        .then().log().all().statusCode(200)
+        .when().get("http://localhost:1080/hello").asString();
+    Assertions.assertThat(body).isEqualTo("AAA");
 
-        body = given().log().all()
-                .then().log().all().statusCode(200)
-                .when().get("http://localhost:1080/hello").asString();
-        Assertions.assertThat(body).isEqualTo("BBB");
+    body = given().log().all()
+        .then().log().all().statusCode(200)
+        .when().get("http://localhost:1080/hello").asString();
+    Assertions.assertThat(body).isEqualTo("BBB");
 
-        String returnStatus = given().log().all()
-                .then().log().all().statusCode(404)
-                .when().get("http://localhost:1080/hello").statusLine();
-        Assertions.assertThat(returnStatus).contains("Not Found");
-    }
+    String returnStatus = given().log().all()
+        .then().log().all().statusCode(404)
+        .when().get("http://localhost:1080/hello").statusLine();
+    Assertions.assertThat(returnStatus).contains("Not Found");
+  }
 
-    @Test
-    void testRegexSyntaxSupport() throws Exception {
-        String orgName = RandomStringUtils.randomAlphabetic(10);
-        Org org = new Org();
-        org.setId(UUID.randomUUID().toString());
-        org.setOrgName(orgName);
+  @Test
+  void testRegexSyntaxSupport() throws Exception {
+    String orgName = RandomStringUtils.randomAlphabetic(10);
+    Org org = new Org();
+    org.setId(UUID.randomUUID().toString());
+    org.setOrgName(orgName);
 
-        String body = new ObjectMapper().writeValueAsString(org);
+    String body = new ObjectMapper().writeValueAsString(org);
 
-        MockServerUtils.client
-                .when(request().withMethod("GET").withPath("/org")
-                        .withQueryStringParameter("orgName", "[A-z]{0,10}"))
-                .respond(response().withStatusCode(200)
-                        .withHeader(new Header("Content-Type", "application/json")).withBody(body));
+    MockServerUtils.client
+        .when(request().withMethod("GET").withPath("/org")
+            .withQueryStringParameter("orgName", "[A-z]{0,10}"))
+        .respond(response().withStatusCode(200)
+            .withHeader(new Header("Content-Type", "application/json")).withBody(body));
 
-        org = given().log().all()
-                .queryParam("orgName", orgName)
-                .then().log().all().statusCode(200)
-                .when().get("http://localhost:1080/org").as(Org.class);
-        Assertions.assertThat(org.getOrgName()).isEqualTo(orgName);
-    }
+    org = given().log().all()
+        .queryParam("orgName", orgName)
+        .then().log().all().statusCode(200)
+        .when().get("http://localhost:1080/org").as(Org.class);
+    Assertions.assertThat(org.getOrgName()).isEqualTo(orgName);
+  }
 
 }
